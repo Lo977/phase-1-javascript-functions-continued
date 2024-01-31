bbbe4fe160fbef6aa0920fe522a388b155619e17
Buddha Lo <lo_977@yahoo.com>
2024-01-30 22:10:57 -0800

    added funtion scope

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(target = "roller-skate") {
+
+  return `This Saturday, I want to ${target}!`
+}
+function mondayWork(target = 'go to the office') {
+  return `This Monday, I will ${target}.`
+}
+
+function wrapAdjective(parameter = "*") {
+  return function(string = "special") {
+    return `You are ${parameter}${string}${parameter}!`
+ 
+ }
+}
